d7d8be29cbd678e611227d8c2632a2ede21547c1
gilad <giladashe@gmail.com>
2020-03-21 20:47:45 +0200

    joystick and flightboard

--- FlightSimulatorApp/views/Joystick.xaml.cs
+++ FlightSimulatorApp/views/Joystick.xaml.cs
@@ -13,58 +13,164 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FlightSimulatorApp.views
+namespace FlightSimulatorApp.Views
 {
     /// <summary>
     /// Interaction logic for Joystick.xaml
     /// </summary>
     public partial class Joystick : UserControl
     {
-        private Point p1 = new Point();
+        /// <summary>Current Aileron</summary>
+        public static readonly DependencyProperty AileronProperty =
+            DependencyProperty.Register("Aileron", typeof(double), typeof(Joystick), null);
+
+        /// <summary>Current Elevator</summary>
+        public static readonly DependencyProperty ElevatorProperty =
+            DependencyProperty.Register("Elevator", typeof(double), typeof(Joystick), null);
+
+        /// <summary>How often should be raised StickMove event in degrees</summary>
+        public static readonly DependencyProperty AileronStepProperty =
+            DependencyProperty.Register("AileronStep", typeof(double), typeof(Joystick), new PropertyMetadata(1.0));
+
+        /// <summary>How often should be raised StickMove event in Elevator units</summary>
+        public static readonly DependencyProperty ElevatorStepProperty =
+            DependencyProperty.Register("ElevatorStep", typeof(double), typeof(Joystick), new PropertyMetadata(1.0));
+
+        /* Unstable - needs work */
+        ///// <summary>Indicates whether the joystick knob resets its place after being released</summary>
+        //public static readonly DependencyProperty ResetKnobAfterReleaseProperty =
+        //    DependencyProperty.Register(nameof(ResetKnobAfterRelease), typeof(bool), typeof(VirtualJoystick), new PropertyMetadata(true));
+
+        /// <summary>Current Aileron in degrees from 0 to 360</summary>
+        public double Aileron
+        {
+            get { return Convert.ToDouble(GetValue(AileronProperty)); }
+            set { SetValue(AileronProperty, value); }
+        }
+
+        /// <summary>current Elevator (or "power"), from 0 to 100</summary>
+        public double Elevator
+        {
+            get { return Convert.ToDouble(GetValue(ElevatorProperty)); }
+            set { SetValue(ElevatorProperty, value); }
+        }
+
+        /// <summary>How often should be raised StickMove event in degrees</summary>
+        public double AileronStep
+        {
+            get { return Convert.ToDouble(GetValue(AileronStepProperty)); }
+            set
+            {
+                if (value < 1) value = 1; else if (value > 90) value = 90;
+                SetValue(AileronStepProperty, Math.Round(value));
+            }
+        }
+
+        /// <summary>How often should be raised StickMove event in Elevator units</summary>
+        public double ElevatorStep
+        {
+            get { return Convert.ToDouble(GetValue(ElevatorStepProperty)); }
+            set
+            {
+                if (value < 1) value = 1; else if (value > 50) value = 50;
+                SetValue(ElevatorStepProperty, value);
+            }
+        }
+
+        /// <summary>Indicates whether the joystick knob resets its place after being released</summary>
+        //public bool ResetKnobAfterRelease
+        //{
+        //    get { return Convert.ToBoolean(GetValue(ResetKnobAfterReleaseProperty)); }
+        //    set { SetValue(ResetKnobAfterReleaseProperty, value); }
+        //}
+
+        /// <summary>Delegate holding data for joystick state change</summary>
+        /// <param name="sender">The object that fired the event</param>
+        /// <param name="args">Holds new values for Aileron and Elevator</param>
+        public delegate void OnScreenJoystickEventHandler(Joystick sender, VirtualJoystickEventArgs args);
+
+        /// <summary>Delegate for joystick events that hold no data</summary>
+        /// <param name="sender">The object that fired the event</param>
+        public delegate void EmptyJoystickEventHandler(Joystick sender);
+
+        /// <summary>This event fires whenever the joystick moves</summary>
+        public event OnScreenJoystickEventHandler Moved;
+
+        /// <summary>This event fires once the joystick is released and its position is reset</summary>
+        public event EmptyJoystickEventHandler Released;
+
+        /// <summary>This event fires once the joystick is captured</summary>
+        public event EmptyJoystickEventHandler Captured;
+
+        private Point _startPos;
+        private double _prevAileron, _prevElevator;
+        private double canvasWidth, canvasHeight;
+        private readonly Storyboard centerKnob;
+
         public Joystick()
         {
             InitializeComponent();
-        }
-        private void CenterKnob_Completed(object sender,EventArgs e)
-        {
-            
+
+            Knob.MouseLeftButtonDown += Knob_MouseLeftButtonDown;
+            Knob.MouseLeftButtonUp += Knob_MouseLeftButtonUp;
+            Knob.MouseMove += Knob_MouseMove;
+
+            centerKnob = Knob.Resources["CenterKnob"] as Storyboard;
         }
 
-        private void Knob_MouseDown(object sender, MouseButtonEventArgs e)
+        private void Knob_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (e.ChangedButton == MouseButton.Left)
-            {
-                p1 = e.GetPosition(this);
-            }
+            _startPos = e.GetPosition(Base);
+            _prevAileron = _prevElevator = 0;
+            canvasWidth = Base.ActualWidth - KnobBase.ActualWidth;
+            canvasHeight = Base.ActualHeight - KnobBase.ActualHeight;
+            Captured?.Invoke(this);
+            Knob.CaptureMouse();
+
+            centerKnob.Stop();
         }
 
         private void Knob_MouseMove(object sender, MouseEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                double x2 = e.GetPosition(sender as Joystick).X;
-                double y2 = e.GetPosition(sender as Joystick).Y;
-                Point p2 = new Point(x2, y2);
-                double length = Length(p1, p2);
-                if (length < Base.Width / 2)
-                {
-                    knobPosition.X = p2.X - p1.X;
-                    knobPosition.Y = p2.Y - p1.Y;
-                }
+            ///!!!!!!!!!!!!!!!!!
+            /// YOU MUST CHANGE THE FUNCTION!!!!
+            ///!!!!!!!!!!!!!!
+            if (!Knob.IsMouseCaptured) return;
 
-            }
+            Point newPos = e.GetPosition(Base);
+
+            Point deltaPos = new Point(newPos.X - _startPos.X, newPos.Y - _startPos.Y);
+
+            double distance = Math.Round(Math.Sqrt(deltaPos.X * deltaPos.X + deltaPos.Y * deltaPos.Y));
+            if (distance >= canvasWidth / 2 || distance >= canvasHeight / 2)
+                return;
+            Aileron = -deltaPos.Y;
+            Elevator = deltaPos.X;
+
+            knobPosition.X = deltaPos.X;
+            knobPosition.Y = deltaPos.Y;
+
+            if (Moved == null ||
+                (!(Math.Abs(_prevAileron - Aileron) > AileronStep) && !(Math.Abs(_prevElevator - Elevator) > ElevatorStep)))
+                return;
+
+            Moved?.Invoke(this, new VirtualJoystickEventArgs { Aileron = Aileron, Elevator = Elevator });
+            _prevAileron = Aileron;
+            _prevElevator = Elevator;
+
         }
 
-        //calculates length between 2 points
-        private double Length(Point first, Point second)
+        private void Knob_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            return Math.Sqrt((second.X - first.X) * (second.X - first.X) + (second.Y - first.Y) * (second.Y - first.Y));
+            Knob.ReleaseMouseCapture();
+            centerKnob.Begin();
         }
 
-        private void Knob_MouseUp(object sender, MouseButtonEventArgs e)
+        private void centerKnob_Completed(object sender, EventArgs e)
         {
-            knobPosition.X = 0;
-            knobPosition.Y = 0;
+            Aileron = Elevator = _prevAileron = _prevElevator = 0;
+            Released?.Invoke(this);
         }
+
     }
 }
